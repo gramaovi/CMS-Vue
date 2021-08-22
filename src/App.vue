1b343d2aeb45e51247a7eddf1dc8d3e3d2b6ee90
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-08-23 02:31:50 +0300

    firbase data push

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
-  
-  <Input></Input>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" />-->
+  <div class="container">
+    <router-view />
+  </div>
 </template>
-
-<script>
-import Input from "./components/Input.vue";
-
-export default {
-  name: "App",
-  components: {
-    Input,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/UserCreate.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card card-body mt-4">
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label>Name</label>
+        <input v-model="form.name" class="form-control" required />
+      </div>
+      <div class="form-group">
+        <label>Prenume</label>
+        <input v-model="form.prenume" class="form-control" required />
+      </div>
+
+      <div class="form-group mt-3">
+        <label>Email</label>
+        <input
+          v-model="form.email"
+          class="form-control"
+          type="email"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-success mt-3">
+        Create User
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { createUser } from '@/firebase'
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const form = reactive({ name: '',prenume:'', email: ''})
+
+    const onSubmit = async () => {
+      await createUser({ ...form })
+      form.name = ''
+      form.email = ''
+    }
+
+    return { form, onSubmit }
+  }
+}
+</script>
